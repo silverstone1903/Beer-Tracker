ab3820aad396be6c410129c5a92b03b3dfb1981f
m-rstewart <mikestewart90@gmail.com>
2016-07-04 10:59:53 -0700

    Added comments

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ var brewdb = new BreweryDb('e49034113c1766216d75b6cb58535cf7');
 var bodyParser = require('body-parser');
 var app = express();
 
+//Stores user check-in info
 var checkIns = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+//Takes the input from the user's search and runs it through the brewdb database
 app.get('/search/:name', function(req, res) {
   brewdb.search.beers({ q: req.params.name, withBreweries: 'Y' }, function(err, data) {
     res.json(data)
   })
 })
 
+//Provides check-in info to be displayed when switching to profile
 app.get('/profile', function(req, res) {
   res.json(checkIns);
 })
 
+//Takes in check-in info from the user and transfers it to checkIns
 app.post('/checkin/:id', function(req, res) {
   brewdb.beer.getById(req.params.id, { withBreweries: 'Y' }, function(err, data) {
     var checkIn = {};
@@ -35,11 +39,13 @@ app.post('/checkin/:id', function(req, res) {
   res.send();
 })
 
+//Test route
 app.post('/clearCheckIn', function(req, res) {
   checkIns = [];
   res.send();
 })
 
+//Test route
 app.post('/showcheckin', function(req,res) {
   console.log(checkIns);
   res.send();
