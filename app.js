fee4c8676cf3fd85e82dfec90ddf3c4a6dc74d56
m-rstewart <mikestewart90@gmail.com>
2016-07-12 13:08:52 -0700

    Simplified port setup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var brewdb = new BreweryDb('e49034113c1766216d75b6cb58535cf7');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 3000;
 
 //Stores user check-in info
 var checkIns = [];
@@ -66,6 +66,4 @@ app.post('/showcheckin', function(req,res) {
   res.send();
 });
 
-app.listen(app.get('port'), function() {
-  console.log("app is running on http://localhost:" + app.get('port'));
-});
+app.listen(port);
