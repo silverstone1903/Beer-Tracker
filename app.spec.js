d738877424f52a9aa458527c3cca44f007fd6684
m-rstewart <mikestewart90@gmail.com>
2016-08-11 13:05:15 -0700

    Added POST test for mocha.  Gulp now watches for any changes in JS files. #13

--- app.spec.js
+++ app.spec.js
@@ -1,2 +1,19 @@
 var assert = require('chai').assert;
 var request = require('request');
+
+describe('Test the REST API', function() {
+  var Client = require('mongodb').MongoClient;
+  var url = 'mongodb://m-rstewart:craft@ds153705.mlab.com:53705/m-rstewart-beer-tracker';
+
+  describe('CREATE', function() {
+    it('expects an ID', function(done) {
+      request({
+        url: 'http://localhost:3000/beer/checkin',
+        method: 'POST'
+      }, function(error, response) {
+        assert.equal(response.statusCode, 404);
+        done();
+      });
+    });
+  });
+});
