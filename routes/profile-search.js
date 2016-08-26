4459a8572190e856339b3e16344b4df0734fa198
m-rstewart <mikestewart90@gmail.com>
2016-08-25 22:57:53 -0700

    Established routes that will handle requests based on beer/brewery selection. #8

--- routes/profile-search.js
+++ routes/profile-search.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var Client = require('mongodb').MongoClient;
+var url = 'mongodb://m-rstewart:craft@ds153705.mlab.com:53705/m-rstewart-beer-tracker';
 var profile = express.Router();
 
-profile.get('/', function(req, res) {
+profile.get('/beer/:beer', function(req, res) {
   res.sendStatus(200);
-})
+});
+
+profile.get('/brewery/:brewery', function(req, res) {
+  res.sendStatus(200);
+});
 
 module.exports = profile;
