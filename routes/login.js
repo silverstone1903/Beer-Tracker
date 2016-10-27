e052e2da27c7bf83c1bd2326421487be85e384c0
m-rstewart <mikestewart90@gmail.com>
2016-10-27 10:16:03 -0700

    Removed the last of the unused cookie code. #17

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var Client = require('mongodb').MongoClient;
 var url = 'mongodb://m-rstewart:craft@ds153705.mlab.com:53705/m-rstewart-beer-tracker';
 var login = express.Router();
 
-login.use(cookieParser());
 login.use(bodyParser.json());
 
 login.post('/', function(req,res) {
@@ -19,7 +17,7 @@ login.post('/', function(req,res) {
       collection.find({ "name": username })
       .toArray(function(error, documents) {
         var results = documents;
-        
+
         if(results[0].password === password) {
           res.send('Success');
           db.close();
